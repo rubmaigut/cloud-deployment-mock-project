58c9d8606d00801783e20bd6a41d37307bed216f
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-25 10:50:49 +0100

    Add DbContext

---
+++ AzureFullstackPractice/Data/PersonDbContext.cs
@@ -0,0 +1,11 @@
+using AzureFullstackPractice.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureFullstackPractice.Data;
+
+public class PersonDbContext: DbContext
+{
+    public PersonDbContext(DbContextOptions options) : base(options) {}
+    
+    public virtual DbSet<Person> Persons { get; set; } 
+}
