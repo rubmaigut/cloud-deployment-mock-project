e9b3273622e5e672bae41e5bb807d34b6de89b96
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-25 11:01:37 +0100

    create httpRequest

--- AzureFullstackPractice/Controllers/PersonsController.cs
+++ AzureFullstackPractice/Controllers/PersonsController.cs
@@ -1,3 +1,5 @@
+using AzureFullstackPractice.Data;
+using AzureFullstackPractice.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzureFullstackPractice.Controllers;
@@ -6,5 +8,19 @@ namespace AzureFullstackPractice.Controllers;
 [Route("[controller]")]
 public class PersonsController : ControllerBase
 {
-    
+    private readonly PersonDbContext _context;
+
+    [HttpGet]
+    public List<Person> GetAll()
+    {
+        return _context.Persons.ToList();
+    }
+
+    [HttpPost("/addPerson")]
+    public ActionResult<Person> AddPerson(Person person)
+    {
+        if (person is null) return BadRequest("wrong information.");
+        _context.Persons.Add(person);
+        return Ok(person);
+    }
 }
